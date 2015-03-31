161bcb2ad3e636662dd56cbcd0d9c86c79892fa9
cdelmoral <carderomad@gmail.com>
2015-03-30 22:45:25 -0500

    generate micropost scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+
   root 'static_pages#home'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330204319) do
+ActiveRecord::Schema.define(version: 20150331033953) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
