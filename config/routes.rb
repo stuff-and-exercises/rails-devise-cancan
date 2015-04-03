50c27122b227c2e39774c9f5c3356dfeab3892ee
cdelmoral <cdelmoral@trustwave.com>
2015-04-03 13:38:24 -0500

    add groups index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'groups/index'
+
   root 'static_pages#home'
 
   devise_for :users
   resources :users
   resources :microposts
+  resources :groups, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
